a344ab91ecb51c8a8499a6d0c0df3e17a2a4d418
miha <stavarachem.msc@gmail.com>
2026-01-15 14:39:50 +0200

    assignment2

--- src/main/java/assignment2/Application.java
+++ src/main/java/assignment2/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.active", "assignment2");
         SpringApplication.run(Application.class, args);
     }
 }
